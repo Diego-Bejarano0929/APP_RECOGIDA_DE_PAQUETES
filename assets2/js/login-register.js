91d253af9a7f741982e89838552e23ede3821bf6
Diego Bejarano <diegobejarano0929@gmail.com>
2022-11-20 22:43:07 -0500

    first commit

---
+++ assets2/js/login-register.js
@@ -0,0 +1,90 @@
+/*
+ *
+ * login-register modal
+ * Autor: Creative Tim
+ * Web-autor: creative.tim
+ * Web script: http://creative-tim.com
+ * 
+ */
+function showRegisterForm(){
+    $('.loginBox').fadeOut('fast',function(){
+        $('.registerBox').fadeIn('fast');
+        $('.login-footer').fadeOut('fast',function(){
+            $('.register-footer').fadeIn('fast');
+        });
+        $('.modal-title').html('Gestión de Paquetes - Registro');
+    }); 
+    $('.error').removeClass('alert alert-danger').html('');
+       
+}
+function showLoginForm(){
+    $('#loginModal .registerBox').fadeOut('fast',function(){
+        $('.loginBox').fadeIn('fast');
+        $('.register-footer').fadeOut('fast',function(){
+            $('.login-footer').fadeIn('fast');    
+        });
+        
+        $('.modal-title').html('Gestión de Paquetes - Login');
+    });       
+     $('.error').removeClass('alert alert-danger').html(''); 
+}
+
+function showTableForm(){
+    $('.loginBox').fadeOut('fast',function(){
+        $('.tablaBox').fadeIn('fast');
+        $('.login-footer').fadeOut('fast',function(){
+            $('.register-footer').fadeOut('fast');
+        });
+        $('.modal-title').html('Gestión de Paquetes - Listado de Ordenes');
+    }); 
+    $('.error').removeClass('alert alert-danger').html('');
+}
+
+function openLoginModal(){
+    showLoginForm();
+    setTimeout(function(){
+        $('#loginModal').modal('show');    
+    }, 230);
+    
+}
+function openRegisterModal(){
+    showRegisterForm();
+    setTimeout(function(){
+        $('#loginModal').modal('show');    
+    }, 230);
+    
+}
+
+function openTableModal(){
+    showTableForm();
+    setTimeout(function(){
+        $('#loginModal').modal('show');    
+    }, 230);
+    
+}
+
+function loginAjax(){
+    /*   Remove this comments when moving to server
+    $.post( "/login", function( data ) {
+            if(data == 1){
+                window.location.replace("/home");            
+            } else {
+                 shakeModal(); 
+            }
+        });
+    */
+
+/*   Simulate error message from the server   */
+     shakeModal();
+}
+
+function shakeModal(){
+    $('#loginModal .modal-dialog').addClass('shake');
+             $('.error').addClass('alert alert-danger').html("Invalid email/password combination");
+             $('input[type="password"]').val('');
+             setTimeout( function(){ 
+                $('#loginModal .modal-dialog').removeClass('shake'); 
+    }, 1000 ); 
+}
+
+   
